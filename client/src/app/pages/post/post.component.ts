4477f57e7bbe33262081a10d64e096e9609063ad
khunichi <joshuapauloynzon@gmail.com>
2024-11-23 18:04:47 +0800

    working create and delete

--- client/src/app/pages/post/post.component.ts
+++ client/src/app/pages/post/post.component.ts
@@ -2,20 +2,31 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { CommonModule } from '@angular/common';
-import { PostService, Post } from './post.service';
+import { PostService, Post_Interface } from './post.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [MatGridListModule, MatCardModule, CommonModule],
+  imports: [MatGridListModule, MatCardModule, CommonModule, ReactiveFormsModule],  // Import ReactiveFormsModule here
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
   changeDetection: ChangeDetectionStrategy.Default
 })
 export class PostComponent implements OnInit {
-  posts: Post[] = [];
+  posts: Post_Interface[] = [];
+  postForm: FormGroup;
+  editingPost: Post_Interface | null = null;
 
-  constructor(private postService: PostService) { }
+  constructor(
+    private postService: PostService,
+    private fb: FormBuilder
+  ) {
+    this.postForm = this.fb.group({
+      title: ['', Validators.required],
+      content: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
     this.fetchPosts();
@@ -30,5 +41,72 @@ export class PostComponent implements OnInit {
         console.error('Error fetching posts:', error);
       }
     });
+  }
+
+  createPost() {
+    if (this.postForm.valid) {
+      const newPost: Post_Interface = {
+        id: 0,
+        title: this.postForm.value.title,
+        content: this.postForm.value.content
+      };
+
+      this.postService.createPost(newPost).subscribe({
+        next: (createdPost) => {
+          this.posts.push(createdPost);
+          this.resetForm();
+        },
+        error: (error) => {
+          console.error('Error creating post:', error);
+        }
+      });
+    }
+  }
+
+  updatePost() {
+    if (this.postForm.valid && this.editingPost) {
+      const updatedPost: Post_Interface = {
+        ...this.editingPost,
+        title: this.postForm.value.title,
+        content: this.postForm.value.content
+      };
+
+      this.postService.updatePost(updatedPost).subscribe({
+        next: (updatedPostData) => {
+          const index = this.posts.findIndex(post => post.id === updatedPostData.id);
+          if (index !== -1) {
+            this.posts[index] = updatedPostData;
+          }
+          this.resetForm();
+        },
+        error: (error) => {
+          console.error('Error updating post:', error);
+        }
+      });
+    }
+  }
+
+  edit(post: Post_Interface) {
+    this.editingPost = post;
+    this.postForm.setValue({
+      title: post.title,
+      content: post.content
+    });
+  }
+
+  deletePost(postId: number) {
+    this.postService.deletePost(postId).subscribe({
+      next: () => {
+        this.posts = this.posts.filter(post => post.id !== postId);
+      },
+      error: (error) => {
+        console.error('Error deleting post:', error);
+      }
+    });
+  }
+
+  resetForm() {
+    this.postForm.reset();
+    this.editingPost = null;
   }
 }
--- client/src/app/pages/post/post.service.ts
+++ client/src/app/pages/post/post.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface Post {
+export interface Post_Interface {
   id: number;
   title: string;
   content: string;
@@ -11,12 +11,30 @@ export interface Post {
 @Injectable({
   providedIn: 'root'
 })
+
 export class PostService {
   private apiUrl = 'http://localhost:3000/post';
 
   constructor(private http: HttpClient) { }
 
-  getAllPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(this.apiUrl);
+  getAllPosts(): Observable<Post_Interface[]> {
+    return this.http.get<Post_Interface[]>(this.apiUrl);
+  }
+
+  createPost(post: Post_Interface): Observable<Post_Interface> {
+    return this.http.post<Post_Interface>(this.apiUrl, {
+      title: post.title,
+      content: post.content
+    });
+  }
+
+  updatePost(post: Post_Interface): Observable<Post_Interface> {
+    const url = `${this.apiUrl}/${post.id}`;
+    return this.http.put<Post_Interface>(url, post);
+  }
+
+  deletePost(id: number): Observable<void> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<void>(url);
   }
 }
